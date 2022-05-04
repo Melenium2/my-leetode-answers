fd4f127fe60cd9e7c4ef2ce0a39b397a4aeddd6a
melenium2 <osumelenium@gmail.com>
2022-05-04 08:01:56 +0500

    add new problem

---
+++ medium/fruit_into_baskets.go
@@ -0,0 +1,73 @@
+package medium
+
+// You are visiting a farm that has a single row of fruit trees arranged from left to right.
+// The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+//
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+//
+// * You only have two baskets, and each basket can only hold a single type of fruit. There is no limit
+// on the amount of fruit each basket can hold.
+// * Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the
+// start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// * Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+//
+//
+//
+// Example 1:
+//
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+//
+// Example 2:
+//
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+//
+// Example 3:
+//
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+//
+//
+// Constraints:
+//
+// 1 <= fruits.length <= 105
+// 0 <= fruits[i] < fruits.length
+func totalFruit(fruits []int) int {
+	if len(fruits) == 0 {
+		return 0
+	}
+
+	var (
+		set        = make(map[int]int, 3)
+		startIndex = 0
+		maxValues  = -1
+	)
+
+	for endIndex := 0; endIndex < len(fruits); endIndex++ {
+		curr := fruits[endIndex]
+		set[curr]++
+
+		for len(set) > 2 {
+			startWindowValue := fruits[startIndex]
+
+			set[startWindowValue]--
+
+			if v := set[startWindowValue]; v == 0 {
+				delete(set, startWindowValue)
+			}
+
+			startIndex++
+		}
+
+		maxValues = max(maxValues, endIndex-startIndex+1)
+	}
+
+	return maxValues
+}
