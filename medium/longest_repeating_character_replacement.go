52d72b1a3e6aed2e8ccfaadedb2f3d6ade2b01d9
melenium2 <osumelenium@gmail.com>
2022-05-20 08:40:09 +0500

    update doc

--- medium/longest_repeating_character_replacement.go
+++ medium/longest_repeating_character_replacement.go
@@ -8,25 +8,25 @@ package medium
 //
 //
 //
-//Example 1:
+// Example 1:
 //
-//Input: s = "ABAB", k = 2
-//Output: 4
-//Explanation: Replace the two 'A's with two 'B's or vice versa.
+// Input: s = "ABAB", k = 2
+// Output: 4
+// Explanation: Replace the two 'A's with two 'B's or vice versa.
 //
-//Example 2:
+// Example 2:
 //
-//Input: s = "AABABBA", k = 1
-//Output: 4
-//Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-//The substring "BBBB" has the longest repeating letters, which is 4.
+// Input: s = "AABABBA", k = 1
+// Output: 4
+// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+// The substring "BBBB" has the longest repeating letters, which is 4.
 //
 //
-//Constraints:
+// Constraints:
 //
-//1 <= s.length <= 105
-//s consists of only uppercase English letters.
-//0 <= k <= s.length
+// 1 <= s.length <= 105
+// s consists of only uppercase English letters.
+// 0 <= k <= s.length
 func characterReplacement(s string, k int) int {
 	if len(s) == 0 {
 		return 0
